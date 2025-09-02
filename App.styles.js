7990ed9335c908e243578313eca4ace94105e57e
Fabrice Piedanna <rs45959@gmail.com>
2025-09-02 15:39:50 +0200

    fin doc 05

--- App.styles.js
+++ App.styles.js
@@ -1,26 +1,15 @@
 import { StyleSheet } from "react-native";
 export const s = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "stretch",
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   box: {
-    // height: "50%",
-    flex: 1,
-  },
-
-  box1: {
-    backgroundColor: "red",
-  },
-
-  box2: {
-    backgroundColor: "blue",
-  },
-
-  box3: {
-    backgroundColor: "green",
+    borderColor: "black",
+    borderWidth: 1,
+    height: 100,
+    width: 100,
   },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,14 +7,8 @@ export default function HomeScreen() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <View style={s.container}>
-          <View style={[s.box, s.box1]}>
+          <View style={s.box}>
             <Text>Box 1 Text</Text>
-          </View>
-          <View style={[s.box, s.box2]}>
-            <Text>Box 2 Text</Text>
-          </View>
-          <View style={[s.box, s.box3]}>
-            <Text>Box 3 Text</Text>
           </View>
         </View>
       </SafeAreaView>
